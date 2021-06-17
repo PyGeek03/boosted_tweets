3c5c419d2589eb625d397ced8de8af65a0fc0229
PyGeek03 <PyGeek03@users.noreply.github.com>
2021-06-17 12:16:42 +0930

    Add twarc1 outputs

--- data_collector_twarc1.py
+++ data_collector_twarc1.py
@@ -9,13 +9,23 @@ consumer_key = os.environ.get("CONSUMER_KEY")
 consumer_secret = os.environ.get("CONSUMER_SECRET")
 access_token = os.environ.get("ACCESS_TOKEN")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
-client = twarc.Twarc(consumer_key, consumer_secret, access_token, access_token_secret)
+client1 = twarc.Twarc(consumer_key, consumer_secret, access_token, access_token_secret)
 
-tweet_ids = ['1404353357469212673', '1404353618518499329']
-retweets = {'data': list(client.retweets(tweet_ids))}
-serialized_json = json.dumps(retweets,
+#tweet_ids = ['1404353357469212673', '1404353618518499329']
+#tweet_ids = ['1405295014327037956']
+#data = {'data': list(client1.retweets(tweet_ids))}
+
+world_trends = client1.trends_place(1)[0]["trends"]
+for i in range(len(world_trends)):
+    if world_trends[i]["tweet_volume"] is None:
+        world_trends[i]["tweet_volume"] = 0
+world_trends.sort(key=lambda trend: -trend["tweet_volume"])
+data = {'data': world_trends}
+
+serialized_json = json.dumps(data,
                              indent=4,
                              sort_keys=True)
 print(serialized_json)
-with open('twarc1_output.json', 'w') as f:
+filename = input("Filename to output to: ")
+with open(f'{filename}.json', 'w') as f:
     f.writelines(serialized_json)
