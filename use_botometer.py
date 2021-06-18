2d821cc78692b1ac4022f246ba79c1fccf5b1478
PyGeek03 <PyGeek03@users.noreply.github.com>
2021-06-19 06:20:24 +0930

    Add cache + commandline arguments

--- use_botometer.py
+++ use_botometer.py
@@ -1,6 +1,8 @@
 import botometer
 import os
 import json
+import argparse
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -24,17 +26,40 @@ bom = botometer.Botometer(wait_on_ratelimit=True,
 
 # Check a sequence of accounts
 #accounts = ["1312662813106724864"]
-conversation_json_file = "lex_tweet.json"
-with open(conversation_json_file, 'r') as f:
-    data = json.load(f)
-tweets = data["data"]
-accounts = set(tweet["author_id"] for tweet in tweets)  # Twitter API v2
+#conversation_json_file = "schumer_tweet-full_conversation-1405321025861165060.json"
 
-botometer_output = {screen_name: result for screen_name, result in bom.check_accounts_in(accounts)}
-serialized_json = json.dumps(botometer_output,
+parser = argparse.ArgumentParser(
+    usage="%(prog)s [FILE]...",
+    description='Use Botometer API to check new accounts.'
+    )
+parser.add_argument('conversation_files', metavar='[FILE]', type=str, nargs='+',
+                    help='one or more json files consisting of the whole conversation associated with a Tweet')
+
+args = parser.parse_args()
+
+accounts = set()
+for file in args.conversation_files:
+    with open(file, 'r') as f:
+        data = json.load(f)
+    tweets = data["data"]
+    accounts |= set(tweet["author_id"] for tweet in tweets)  # Twitter API v2
+
+output_file = "outputs/botometer_cache.json"
+
+with open(output_file, 'r') as f:
+    all_output = json.load(f)
+prev_accounts = set(all_output)
+
+new_accounts = accounts - prev_accounts
+if len(new_accounts) > 2000:  # Botometer API's free daily limit
+    new_accounts = set(list(new_accounts)[:2000])
+new_output = {screen_name: result for screen_name, result in bom.check_accounts_in(new_accounts)}
+all_output.update(new_output)
+
+serialized_json = json.dumps(all_output,
                              indent=4,
                              sort_keys=True)
 print(serialized_json)
-filename = input("Filename to output to: ")
-with open(f'{filename}.json', 'w') as f:
+print(f"Total num. of accounts checked: {len(all_output)}")
+with open(output_file, 'w') as f:
     f.writelines(serialized_json)
